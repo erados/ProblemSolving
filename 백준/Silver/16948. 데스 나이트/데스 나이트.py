186439e5ca1215d6b5fee942f1bd03030792cdc3
Myungwoo Song <imdiots@gmail.com>
2023-02-07 08:57:04 +0900

    [Silver I] Title: 데스 나이트, Time: 88 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -4,23 +4,23 @@ N = int(input())
 r1, c1, r2, c2 = map(int, input().split())
 
 moves = [(-1, -2), (1, -2), (-1, 2), (1, 2), (-2, 0), (2, 0)]
-q = deque([(r1, c1, 0)])
-visited = [[False] * N for _ in range(N)]
+q = deque([(r1, c1)])
+visited = [[0] * N for _ in range(N)]
 
 while q:
-    y, x, time = q.popleft()
+    y, x = q.popleft()
 
     for dx, dy in moves:
         nx = x + dx
         ny = y + dy
 
         if 0 <= nx < N and 0 <= ny < N:
-            if (ny, nx) == (r2, c2):
-                print(time + 1)
+            if ny == r2 and nx == c2:
+                print(visited[y][x] + 1)
                 exit()
 
             if not visited[ny][nx]:
-                q.append((ny, nx, time + 1))
-                visited[ny][nx] = True
+                q.append((ny, nx))
+                visited[ny][nx] = visited[y][x] + 1
 
 print(-1)
