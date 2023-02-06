54f942d526f5e1f07dc2893998a1e6360cf438e2
Myungwoo Song <imdiots@gmail.com>
2023-02-06 15:43:27 +0900

    [Bronze II] Title: OX퀴즈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,11 +1,8 @@
-from sys import stdin
+import sys
 
-input = stdin.readline
-
-for _ in range(int(input())):
-    ans = input().rstrip().split("X")
+for _ in range(int(sys.stdin.readline())):
     score = 0
-    for c in ans:
+    for c in sys.stdin.readline()[:-1].split("X"):
         score += len(c) * (len(c) + 1) // 2
 
     print(score)
