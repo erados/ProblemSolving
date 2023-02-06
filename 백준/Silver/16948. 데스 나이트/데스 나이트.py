29a34a40f194538fe3a7c3f669180f230bc9e24a
Myungwoo Song <imdiots@gmail.com>
2023-02-07 08:58:39 +0900

    [Silver I] Title: 데스 나이트, Time: 88 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -3,16 +3,18 @@ from collections import deque
 N = int(input())
 r1, c1, r2, c2 = map(int, input().split())
 
-moves = [(-1, -2), (1, -2), (-1, 2), (1, 2), (-2, 0), (2, 0)]
+dxs = [-1, 1, -1, 1, -2, 2]
+dys = [-2, -2, 2, 2, 0, 0]
+
 q = deque([(r1, c1)])
 visited = [[0] * N for _ in range(N)]
 
 while q:
     y, x = q.popleft()
 
-    for dx, dy in moves:
-        nx = x + dx
-        ny = y + dy
+    for i in range(6):
+        nx = x + dxs[i]
+        ny = y + dys[i]
 
         if 0 <= nx < N and 0 <= ny < N:
             if ny == r2 and nx == c2:
