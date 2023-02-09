7a9c2a5150ba758d43d727037c6a11419e362382
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-09 17:08:58 +0900

    [Gold III] Title: 크게 만들기, Time: 180 ms, Memory: 34384 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -6,7 +6,7 @@ N, K = map(int, input().split())
 
 stack = []
 count = 0
-for index, n in enumerate(input()[:-1]):
+for n in input()[:-1]:
     while stack and stack[-1] < n and count < K:
         stack.pop()
         count += 1
