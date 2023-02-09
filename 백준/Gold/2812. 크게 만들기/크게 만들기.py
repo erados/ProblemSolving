8f4dd7fe50014c59759f2d37c6249106d2dcdfc1
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-09 17:11:01 +0900

    [Gold III] Title: 크게 만들기, Time: 108 ms, Memory: 34384 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -4,13 +4,17 @@ input = stdin.readline
 
 N, K = map(int, input().split())
 
-stack = []
-count = 0
-for n in input().rstrip():
-    while stack and stack[-1] < n and count < K:
-        stack.pop()
-        count += 1
-    stack.append(n)
+
+def solve():
+    stack = []
+    count = 0
+    for n in input().rstrip():
+        while stack and stack[-1] < n and count < K:
+            stack.pop()
+            count += 1
+        stack.append(n)
+
+    print("".join(stack)[: N - K])
 
 
-print("".join(stack)[: N - K])
+solve()
