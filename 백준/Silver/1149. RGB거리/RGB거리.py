d3861bd8948a1f3c0851a112147e2ee7e10a65dc
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-11 15:34:55 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,20 +4,18 @@ input = stdin.readline
 
 N = int(input())
 
-dp = []
-
 
 def _min(a, b):
     return b if b < a else a
 
 
-dp = list(map(int, input().split()))
+r, g, b = map(int, input().split())
 for i in range(1, N):
-    cost = list(map(int, input().split()))
-    dp = [
-        cost[0] + _min(dp[1], dp[2]),
-        cost[1] + _min(dp[0], dp[2]),
-        cost[2] + _min(dp[0], dp[1]),
-    ]
+    c_r, c_g, c_b = map(int, input().split())
+    r, g, b = (
+        c_r + _min(g, b),
+        c_g + _min(r, b),
+        c_b + _min(r, g),
+    )
 
-print(min(dp))
+print(min(r, g, b))
