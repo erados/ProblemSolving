6bc55e0b24cc9ca4c616ae8ecaf028fc01cd9600
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-12 14:56:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 108 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -3,22 +3,10 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# S, E = map(int, input().split())
-def solve(S, E):
 
-    b_S, b_E = bin(S)[2:], bin(E)[2:]
+def solve2():
+    S, E = map(int, input().split())
 
-    if S >= E:
-        return S - E
-
-    print(b_S, b_E)
-
-    return min(
-        abs(int(b_E[: len(b_S)], 2) - int(b_S, 2)), (int(b_E[: len(b_S)] + "0", 2) - S)
-    ) + b_E[len(b_S) :].count("1")
-
-
-def solve2(S, E):
     q = deque([(0, S)])
     visited = [False] * 200001
     visited[S] = True
@@ -26,7 +14,8 @@ def solve2(S, E):
     while q:
         time, now = q.popleft()
         if now == E:
-            return time
+            print(time)
+            return
         if now < E:
             if not visited[now * 2]:
                 q.appendleft((time, now * 2))
@@ -39,5 +28,4 @@ def solve2(S, E):
             visited[now - 1] = True
 
 
-S, E = map(int, input().split())
-print(solve2(S, E))
+solve2()
