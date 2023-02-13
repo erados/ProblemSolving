c1bdb41955b5fd7a63bb82c831285fda8e38ca35
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-13 16:32:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.02 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,17 +1,18 @@
-from collections import defaultdict
 
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
-    name_to_num = {name : index for index, name in enumerate(id_list)}
-    graph = defaultdict(set)
-    
+    name_to_num = {k: v for v, k in enumerate(id_list)}
+    graph = [[False] * len(id_list) for i in range(len(id_list))]
+
     for item in report:
-        a, b = item.split()
-        graph[b].add(a)
+        a, b = (name_to_num[k] for k in item.split())
+        graph[b][a] = True
+
     for b in graph:
-        if len(graph[b]) >= k:
-            for a in graph[b]:
-                answer[name_to_num[a]] += 1
-    
-    
+        if sum(b) >= k:
+            for index, value in enumerate(b):
+                if value:
+                    answer[index] += 1
+
+
     return answer
