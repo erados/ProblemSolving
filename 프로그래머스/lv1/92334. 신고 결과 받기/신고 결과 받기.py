3efffc41973bb3124196c50013354abba8879de6
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-13 16:19:04 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,17 @@
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    name_to_num = {k: v for v, k in enumerate(id_list)}
+    graph = [[False] * len(id_list) for i in range(len(id_list))]
+    
+    for item in report:
+        a, b = (name_to_num[k] for k in item.split())
+        graph[b][a] = True
+    
+    for b in graph:
+        if sum(b) >= k:
+            for index, value in enumerate(b):
+                if value:
+                    answer[index] += 1
+    
+    
+    return answer
