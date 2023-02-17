808037491cc9bbb8887902859eb230919e18f9c9
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-17 15:00:40 +0900

    [Silver II] Title: N과 M (9), Time: 132 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,4 +1,5 @@
 from sys import stdin
+from itertools import permutations
 from collections import defaultdict
 
 input = stdin.readline
@@ -8,29 +9,7 @@ N, M = map(int, input().split())
 nums = sorted([*map(int, input().split())])
 visited = defaultdict(bool)
 
-
-ans = []
-selected = {k: False for k in range(len(nums))}
-
-
-def permutation(m):
-
-    if m == 0:
-        yield tuple(ans)
-        return
-
-    for index in range(len(nums)):
-        if not selected[index]:
-            ans.append(nums[index])
-            selected[index] = True
-            yield from permutation(m - 1)
-            selected[index] = False
-            ans.pop()
-
-    return
-
-
-for comb in permutation(M):
+for comb in permutations(nums, M):
     if not visited[comb]:
         print(*comb)
         visited[comb] = True
