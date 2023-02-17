89193c146520581c274a001de1a6abff5dc2fb77
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-17 14:39:48 +0900

    [Gold IV] Title: 문자열 폭발, Time: 348 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -4,12 +4,11 @@ input = stdin.readline
 
 word = input().rstrip()
 bomb = [c for c in input().rstrip()]
-
+bomb_last = bomb[-1]
 stack = []
 
 for c in word:
     stack.append(c)
-    if c == bomb[-1]:
-        if stack[len(stack) - len(bomb) :] == bomb:
-            del stack[-len(bomb) :]
+    if c == bomb_last and stack[len(stack) - len(bomb) :] == bomb:
+        del stack[-len(bomb) :]
 print("".join(stack) if stack else "FRULA")
