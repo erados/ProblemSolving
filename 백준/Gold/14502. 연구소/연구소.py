d21944be4d39ceb1af98bd1049c735696353a2e1
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-18 15:51:17 +0900

    [Gold IV] Title: 연구소, Time: 1836 ms, Memory: 34200 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,66 @@
+from sys import stdin
+from itertools import combinations
+from collections import deque
+
+input = stdin.readline
+
+N, M = map(int, input().split())
+graph = []
+moves = [
+    (1, 0),
+    (-1, 0),
+    (0, 1),
+    (0, -1),
+]
+
+pos_empty = []
+pos_twos = []
+for n in range(N):
+    row = input().split()
+    for m in range(M):
+        if row[m] == "0":
+            pos_empty.append((n, m))
+        elif row[m] == "2":
+            pos_twos.append((n, m))
+
+    graph.append(row)
+
+
+def bfs():
+    q = deque(pos_twos)
+    visited = set()
+
+    while q:
+        y, x = q.popleft()
+        for dy, dx in moves:
+            ny = y + dy
+            nx = x + dx
+
+            if (
+                0 <= ny < N
+                and 0 <= nx < M
+                and graph[ny][nx] == "0"
+                and (ny, nx) not in visited
+            ):
+                visited.add((ny, nx))  # set 과 dict 과 list 성능차이 ?
+                
+                if len(visited) > min_cnt_twos:
+                    return 100
+                
+                q.append((ny, nx))
+    return len(visited)
+
+
+min_cnt_twos = 100
+for comb in combinations(pos_empty, 3):
+    for y, x in comb:
+        graph[y][x] = "1"
+
+    temp = bfs()
+    if min_cnt_twos > temp:
+        min_cnt_twos = temp
+
+    for y, x in comb:
+        graph[y][x] = "0"
+
+print(len(pos_empty) - 3 - min_cnt_twos)
