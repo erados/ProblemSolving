2668e71f3ec8bc30009658c2f10387b5b8f9c60a
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-22 14:52:15 +0900

    [Gold II] Title: 보석 도둑, Time: 1120 ms, Memory: 91804 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -5,23 +5,26 @@ input = stdin.readline
 
 N, K = map(int, input().split())
 
-main_heap = []
+jewels = []
 for n in range(N):
     m, v = map(int, input().split())
-    heappush(main_heap, (m, v))
+    jewels.append((v, m))
+jewels.sort(key=lambda x: x[1])
 
+bags = []
 for k in range(K):
-    heappush(main_heap, (int(input()), 1_000_001))
+    bags.append(int(input()))
+bags.sort()
 
 answer = 0
-temp_heap = []
-while main_heap:
-    m, v = heappop(main_heap)
+heap = []
+jewel_idx = 0
+for bag in bags:
+    while jewel_idx < len(jewels) and jewels[jewel_idx][1] <= bag:
+        heappush(heap, -jewels[jewel_idx][0])
+        jewel_idx += 1
 
-    if v != 1_000_001:
-        heappush(temp_heap, (-v, m))
-
-    elif temp_heap:
-        answer += heappop(temp_heap)[0]
+    if heap:
+        answer += heappop(heap)
 
 print(-answer)
