c2b4ba192c90229a62ec4ed3dfa513e297c32183
Myungwoo Song <myungwoo.tech@gmail.com>
2023-02-28 12:29:19 +0900

    [Gold V] Title: 토마토, Time: 740 ms, Memory: 309912 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,55 +1,37 @@
-from sys import stdin
 from collections import deque
+import sys
 
-input = stdin.readline
+input = sys.stdin.readline
 
 M, N, H = map(int, input().split())
-graph = [[] for _ in range(H)]
-moves = [
-    (1, 0, 0),
-    (-1, 0, 0),
-    (0, 1, 0),
-    (0, -1, 0),
-    (0, 0, 1),
-    (0, 0, -1),
-]
 
-tomatos = []
-last_tomatos = 0
+tomatos = [[list(map(int, input().split())) for n in range(N)] for h in range(H)]
+
+moves = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+to_visit = deque()
+
 for h in range(H):
     for n in range(N):
-        row = list(map(int, input().split()))
-        for cell_idx in range(M):
-            if row[cell_idx] == 1:
-                tomatos.append((h, n, cell_idx, 0))
-            elif row[cell_idx] == 0:
-                last_tomatos += 1
-        graph[h].append(row)
+        for m in range(M):
+            if tomatos[h][n][m] == 1:
+                to_visit.append((m, n, h, 0))
 
+while to_visit:
+    target = to_visit.popleft()
+    m, n, h, time = target
+    for move in moves:
+        nm = m + move[0]
+        nn = n + move[1]
+        nh = h + move[2]
+        ntime = time + 1
+        if 0 <= nm < M and 0 <= nn < N and 0 <= nh < H:
+            if tomatos[nh][nn][nm] == 0:
+                tomatos[nh][nn][nm] = 1
+                to_visit.append((nm, nn, nh, ntime))
 
-def bfs():
-    global last_tomatos
-    q = deque(tomatos)
-    time = 0
-    if last_tomatos:
-        while q:
-            z, y, x, time = q.popleft()
-
-            for dz, dy, dx in moves:
-                nz = z + dz
-                ny = y + dy
-                nx = x + dx
-
-                if 0 <= nz < H and 0 <= ny < N and 0 <= nx < M:
-                    if graph[nz][ny][nx] == 0:
-                        graph[nz][ny][nx] = 1
-                        last_tomatos -= 1
-                        q.append((nz, ny, nx, time + 1))
-
-    if last_tomatos:
-        return -1
-
-    return time
-
-
-print(bfs())
+num_of_zero = sum(sum(tomatos, []), []).count(0)
+if num_of_zero > 0:
+    print(-1)
+else:
+    print(time)
