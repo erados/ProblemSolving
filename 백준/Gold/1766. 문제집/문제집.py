63b665a03a4f725a5140b10b92e64747bf71eba1
Myungwoo Song <myungwoo.tech@gmail.com>
2023-03-25 18:35:17 +0900

    [Gold II] Title: 문제집, Time: 192 ms, Memory: 42564 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -3,27 +3,32 @@ from heapq import heappop, heappush
 
 input = stdin.readline
 
-N, M = map(int, input().split())
-# 남은 부모 노드의 수, 다음 노드의 인덱스
-graph = [[0, []] for _ in range(N + 1)]
-graph[0][0] -= 1
 
-for m in range(M):
-    a, b = map(int, input().split())
-    graph[b][0] += 1
-    graph[a][1].append(b)
+def solve():
+    N, M = map(int, input().split())
+    # 남은 부모 노드의 수, 다음 노드의 인덱스
+    graph = [[0, []] for _ in range(N + 1)]
+    graph[0][0] -= 1
 
-heap = []
-for idx in range(1, N + 1):
-    p, _ = graph[idx]
-    if p == 0:
-        heappush(heap, idx)
-answer = []
-while heap:
-    idx = heappop(heap)
-    answer.append(idx)
-    for next_idx in graph[idx][1]:
-        graph[next_idx][0] -= 1
-        if graph[next_idx][0] == 0:
-            heappush(heap, next_idx)
-print(*answer)
+    for m in range(M):
+        a, b = map(int, input().split())
+        graph[b][0] += 1
+        graph[a][1].append(b)
+
+    heap = []
+    for idx in range(1, N + 1):
+        p, _ = graph[idx]
+        if p == 0:
+            heappush(heap, idx)
+    answer = []
+    while heap:
+        idx = heappop(heap)
+        answer.append(idx)
+        for next_idx in graph[idx][1]:
+            graph[next_idx][0] -= 1
+            if graph[next_idx][0] == 0:
+                heappush(heap, next_idx)
+    print(*answer)
+
+
+solve()
